21357ef3240190fae866ea5313a44c201d17bc9b
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-10-04 10:05:45 +0200

    updated scripts

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -3,10 +3,10 @@
 deepspeed --include localhost:0,1 --master_port=29503 llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --model_name_or_path /leonardo_scratch/large/userexternal/fgaragna/models/lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/llava-665k/llava_v1_5_mix665k.json \
-    --image_folder ./playground/llava-665k \
+    --data_path /leonardo_scratch/large/userexternal/fgaragna/dataset/viral/llava_v1_5_mix665k.json \
+    --image_folder /leonardo_scratch/large/userexternal/fgaragna/dataset/viral \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
@@ -16,7 +16,7 @@ deepspeed --include localhost:0,1 --master_port=29503 llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-7b-instruct-viral \
+    --output_dir /leonardo_scratch/large/userexternal/fgaragna/checkpoints/viral \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
