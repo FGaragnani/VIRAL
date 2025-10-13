37da5aef8c7d5849b414ce8cb5dbae5463d8fdb8
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-10-14 01:58:25 +0200

    Update llava_llama.py

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -693,13 +693,13 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
                 image_sizes=image_sizes
             )
         else:
-            print("Warning: images are None for generation in LLavaLLamaForCausalLM.")
             inputs_embeds = self.get_model().embed_tokens(inputs)
 
         return super().generate(
             position_ids=position_ids,
             attention_mask=attention_mask,
             inputs_embeds=inputs_embeds,
+            images=images,
             **kwargs
         )
 
