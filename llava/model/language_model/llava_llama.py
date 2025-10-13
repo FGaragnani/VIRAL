8d06912964c424b259527249b27935262ad2eecf
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-10-13 20:12:37 +0200

    Update llava_llama.py

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -408,6 +408,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         images: Optional[torch.FloatTensor] = None,
         image_sizes: Optional[List[List[int]]] = None,
         return_dict: Optional[bool] = None,
+        cache_position = None,
         is_coco = None,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         r"""
