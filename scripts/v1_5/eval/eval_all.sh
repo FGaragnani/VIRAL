02c79815ddb191b7ee2abba2dc06d31cfd1cb05f
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-10-14 23:49:45 +0200

    Update eval_all.sh

--- scripts/v1_5/eval/eval_all.sh
+++ scripts/v1_5/eval/eval_all.sh
@@ -39,7 +39,7 @@ export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION="python"
 output_dir="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/viral/${run_name}"
 
 # task_list=(gqa scienceqa_img mmmu_val seedbench ai2d textvqa_val)
-task_list=(seedbench textvqa_val)
+task_list=(seedbench scienceqa_img)
 echo ${task_list[$SLURM_ARRAY_TASK_ID]}
 
 checkpoint_path="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/viral/viral_llava_lora/"
