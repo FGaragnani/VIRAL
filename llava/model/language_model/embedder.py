7a804e647d58faf194678e2eafc4e23202e152eb
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-11-15 16:22:41 +0100

    Update embedder.py

--- llava/model/language_model/embedder.py
+++ llava/model/language_model/embedder.py
@@ -60,8 +60,6 @@ class PatchEmbedder(nn.Module):
             model_name = "/work/cvcs2025/garagnani_napolitano_ricciardi/fil/tesi/checkpoints/facebook/dinov2-base"
             self.processor = AutoImageProcessor.from_pretrained(model_name)
             self.model = AutoModel.from_pretrained(model_name).to(device)
-        self.processor.to(device)
-        self.model.to(device)
         self.model.eval()
         self.agg_mode = agg_mode
         self.device = device
